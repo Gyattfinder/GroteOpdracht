d9582a0fe5776a3c0e6ba7649522d17b3b73916f
Gyattfinder <brent.hoozee@jopie.be>
2025-04-24 12:02:22 +0200

    Initial Commit

---
+++ DrankCalc/DrankCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrankCalcApp.swift
+//  DrankCalc
+//
+//  Created by Brent Hoozee on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct DrankCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
